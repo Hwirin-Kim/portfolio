993b023ddd0b24df1f7da7f52980f1200f0710d3
Hwirin Kim <kimhuirin@gmail.com>
2025-12-04 09:56:29 +0900

    fix: 경로 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,44 +62,59 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon.ico/favicon.ico" },
+      { url: `${BASE_URL}/favicon.ico/favicon.ico` },
       {
-        url: "/favicon.ico/favicon-16x16.png",
+        url: `${BASE_URL}/favicon.ico/favicon-16x16.png`,
         sizes: "16x16",
         type: "image/png",
       },
       {
-        url: "/favicon.ico/favicon-32x32.png",
+        url: `${BASE_URL}/favicon.ico/favicon-32x32.png`,
         sizes: "32x32",
         type: "image/png",
       },
       {
-        url: "/favicon.ico/favicon-96x96.png",
+        url: `${BASE_URL}/favicon.ico/favicon-96x96.png`,
         sizes: "96x96",
         type: "image/png",
       },
     ],
     apple: [
-      { url: "/favicon.ico/apple-icon.png" },
-      { url: "/favicon.ico/apple-icon-57x57.png", sizes: "57x57" },
-      { url: "/favicon.ico/apple-icon-60x60.png", sizes: "60x60" },
-      { url: "/favicon.ico/apple-icon-72x72.png", sizes: "72x72" },
-      { url: "/favicon.ico/apple-icon-76x76.png", sizes: "76x76" },
-      { url: "/favicon.ico/apple-icon-114x114.png", sizes: "114x114" },
-      { url: "/favicon.ico/apple-icon-120x120.png", sizes: "120x120" },
-      { url: "/favicon.ico/apple-icon-144x144.png", sizes: "144x144" },
-      { url: "/favicon.ico/apple-icon-152x152.png", sizes: "152x152" },
-      { url: "/favicon.ico/apple-icon-180x180.png", sizes: "180x180" },
+      { url: `${BASE_URL}/favicon.ico/apple-icon.png` },
+      { url: `${BASE_URL}/favicon.ico/apple-icon-57x57.png`, sizes: "57x57" },
+      { url: `${BASE_URL}/favicon.ico/apple-icon-60x60.png`, sizes: "60x60" },
+      { url: `${BASE_URL}/favicon.ico/apple-icon-72x72.png`, sizes: "72x72" },
+      { url: `${BASE_URL}/favicon.ico/apple-icon-76x76.png`, sizes: "76x76" },
+      {
+        url: `${BASE_URL}/favicon.ico/apple-icon-114x114.png`,
+        sizes: "114x114",
+      },
+      {
+        url: `${BASE_URL}/favicon.ico/apple-icon-120x120.png`,
+        sizes: "120x120",
+      },
+      {
+        url: `${BASE_URL}/favicon.ico/apple-icon-144x144.png`,
+        sizes: "144x144",
+      },
+      {
+        url: `${BASE_URL}/favicon.ico/apple-icon-152x152.png`,
+        sizes: "152x152",
+      },
+      {
+        url: `${BASE_URL}/favicon.ico/apple-icon-180x180.png`,
+        sizes: "180x180",
+      },
     ],
     other: [
       {
         rel: "mask-icon",
-        url: "/favicon.ico/apple-icon.png",
+        url: `${BASE_URL}/favicon.ico/apple-icon.png`,
         color: "#22c55e",
       },
     ],
   },
-  manifest: "/favicon.ico/manifest.json",
+  manifest: `${BASE_URL}/favicon.ico/manifest.json`,
   metadataBase: new URL(BASE_URL),
 };
 
